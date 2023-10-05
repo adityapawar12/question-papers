8b6712dde3a4bfb2085fa241ee40824aaa46362b
Nikhil <nikhil@tikao.in>
2023-10-05 23:43:23 +0530

    Added a mast head section.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,43 @@
 import Navbar from "@/components/shared/navbar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Papers from "./_components/papers";
+import Image from "next/image";
+import { useRef } from "react";
 
 export default function Home() {
+  const questionPapersRef = useRef<HTMLDivElement | null>(null);
+
   return (
     <>
       <Navbar />
-      <div className="flex justify-center mt-6">
-        <div className="max-w-screen-lg w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] flex justify-between p-4">
+      <div className={`w-full relative text-right flex justify-center`}>
+        <Image
+          src={"/edu-pic.jpg"}
+          alt="Education Picture"
+          width={1920}
+          height={1080}
+          className="h-[40rem] w-full object-cover"
+        ></Image>
+        <h1 className="text-emerald-700 max-sm:right-4 max-lg:right-10 text-5xl md:text-6xl lg:text-8xl w-[1024px] absolute top-36 md:top-20 flex justify-end flex-col max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg font-sans font-bold">
+          Start Your <br /> Learning <br /> Journey <br />
+          With Us.
+          <button
+            className={`text-base md:text-xl lg:text-2xl rounded-lg md:rounded-xl mt-2 md:mt-4 bg-emerald-400 text-white p-2 md:p-4 block self-end`}
+            onClick={() => {
+              if (questionPapersRef.current) {
+                questionPapersRef.current.scrollIntoView({
+                  behavior: "smooth",
+                  block: "end",
+                });
+              }
+            }}
+          >
+            Get Question Papers
+          </button>
+        </h1>
+      </div>
+      <div ref={questionPapersRef} className="flex justify-center">
+        <div className="max-w-screen-lg w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] flex justify-between">
           <Tabs
             className="max-w-screen-lg w-full sm:w-[640px] md:w-[768px] lg:w-[1024px]"
             defaultValue="marathi"
--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-center bg-emerald-200 h-16 items-center border-b-2 border-emerald-100 shadow-emerald-100 shadow-lg text-emerald-900">
+    <div className="w-full flex justify-center bg-emerald-200 h-16 items-center border-b-0 border-emerald-100 shadow-emerald-100 shadow-lg text-emerald-900">
       <div className="max-w-screen-lg w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] flex justify-between p-4">
         <div className="text-xl font-sans font-semibold">Question Papers</div>
       </div>
